aea182772c62b356e345d52de1d969a91c585457
Vinay Sikarwar <svinay.1987@gmail.com>
2020-11-25 13:37:17 +0530

    Added notify url in system config

---
+++ Block/System/Config/Form/Field/Disable.php
@@ -0,0 +1,16 @@
+<?php
+namespace Payout\Payment\Block\System\Config\Form\Field;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Disable extends \Magento\Config\Block\System\Config\Form\Field
+{    
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $element->setDisabled('disabled');
+		$url  = $this->getBaseUrl().'Payout/redirect/order';
+        $element->setValue($url);
+        return $element->getElementHtml();
+
+    }
+}
